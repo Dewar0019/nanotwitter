1c655bed369931f73ed43169bc4a6895d1073307
lttviet <lttviet@gmail.com>
2015-10-16 15:02:45 -0400

    placeholders

--- app.rb
+++ app.rb
@@ -5,7 +5,19 @@ require 'require_all'
 require_all './models'
 
 
-get '/' do #form for person
-	@user = User.all
-	erb :index
+get '/' do
+  erb :index
+end
+
+get '/login' do
+  erb :login
+end
+
+get '/signup' do
+  erb :signup
+end
+
+get '/users' do
+  @users = User.all
+  erb :users
 end
