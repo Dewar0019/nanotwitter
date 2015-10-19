007b2e4324d0cea3daeddb5f6931f7806510081f
Shraddha Basnyat <sbasnyat@brandeis.edu>
2015-10-19 16:48:32 -0400

    Basic Authentication
    If someone is logged in, then index page says welcome email address of
    the user in the nav bar otherwise non logged in users see sign up and
    sign in page. Sign up also works

--- app.rb
+++ app.rb
@@ -5,19 +5,57 @@ require 'require_all'
 require_all './models'
 
 
+enable :sessions
+
+helpers do
+
+	def login? 
+		!(session[:email].nil?)
+	end
+
+end
+
+
 get '/' do
-  erb :index
+	erb :index
 end
 
 get '/login' do
   erb :login
 end
 
+post '/login' do
+	if User.exists?(email: params[:email]) 
+		@user = User.find_by(email: params[:email])
+		if @user.password == params[:password]
+			session[:email] = params[:email]
+			redirect '/'
+		end
+	end
+	erb :error
+end
+
+get '/logout' do
+  session[:email] = nil
+  redirect "/"
+end
+
 get '/signup' do
   erb :signup
+end
+
+post '/signup' do
+	user = User.create(user_name: params[:user_name], password: params[:password], email: params[:email])
+	if user.save
+		session[:email] = params[:email]
+		redirect '/'
+	end
+	erb :error
 end
 
 get '/users' do
   @users = User.all
   erb :users
 end
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
 require 'fabrication'
 
+User.delete_all
+Tweet.delete_all
 
 100.times { Fabricate(:user) }
 100.times { Fabricate(:tweet) }
+
+
