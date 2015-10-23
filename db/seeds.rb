ee9be5cd4899d90a9ca54eb410e396c01dc292e6
lttviet <lttviet@gmail.com>
2015-10-23 01:08:30 -0400

    seed for following, retweet and favorite

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@ Tweet.delete_all
 
 100.times { Fabricate(:user) }
 100.times { Fabricate(:tweet) }
+
+# seed following
+User.all.each do |u|
+  random_number = rand(20)
+
+  followings = (0..100).to_a.sample(random_number)
+  followings.delete(u.id)
+
+  followings.each do |f|
+    Follow.create(user_id: u.id, following_id: f)
+  end
+end
+
+# seed retweet and favorite
+Tweet.all.each do |t|
+  random_number = rand(20)
+
+  retweeters = (0..100).to_a.sample(random_number)
+  retweeters.delete(t.user_id)
+
+  favorites = (0..100).to_a.sample(random_number)
+  favorites.delete(t.user_id)
+
+  retweeters.each do |r|
+    Retweet.create(tweet_id: t.id, user_id: r)
+  end
+
+  favorites.each do |f|
+    Favorite.create(tweet_id: t.id, user_id: f)
+  end
+end
