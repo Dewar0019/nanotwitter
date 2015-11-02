049d82e306692988c1a07851deb17059423ba5e9
Subahu Rayamajhi <subahur@brandeis.edu>
2015-11-01 22:49:50 -0500

    more int test

--- test/integration/test.rb
+++ test/integration/test.rb
@@ -25,5 +25,15 @@ class MyTest < MiniTest::Test
     assert page.has_no_content?('DOESNOT EXIST'), "Sign up path failed"
   end
 
+  def test_signin_and_tweet
+    visit '/login'
+    page.fill_in 'email', :with => 'testuser892@test.com'
+    page.fill_in 'password', :with => 'testuser892' 
+    click_button('Login')
+    page.fill_in 'tweet', :with => 'new test tweet'
+    click_button('Tweet')
+    assert page.has_content?('new test tweet'), "Test failed, Tweet should post"
+  end
+
 
 end
