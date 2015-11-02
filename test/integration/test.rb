363620778420e77d341067cbb8c70fc6c0afb339
Subahu Rayamajhi <subahur@brandeis.edu>
2015-11-01 19:50:12 -0500

    more integration test with capybara

--- test/integration/test.rb
+++ test/integration/test.rb
@@ -11,6 +11,19 @@ class MyTest < MiniTest::Test
 
   def test_root_path_works
     visit '/'
-    assert page.has_content?('hello'), "custom fail msg"
+    assert page.has_content?('hello'), "home page path failed"
   end
+
+
+  def test_root_path_signup
+    visit '/signup'
+    assert page.has_content?('Sign up'), "Sign up path failed"
+  end
+
+  def test_root_path_no_content
+    visit '/signup'
+    assert page.has_no_content?('DOESNOT EXIST'), "Sign up path failed"
+  end
+
+
 end
