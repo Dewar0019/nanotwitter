dc8698faa24707695cf9a71fd41dfe43dd701f20
lttviet <lttviet@gmail.com>
2015-11-08 22:07:21 -0500

    simplify environment.rb

--- config/environments.rb
+++ config/environments.rb
@@ -1,27 +1,8 @@
-# These Settings Establish the Proper Database Connection for Heroku Postgres
-# The environment variable DATABASE_URL should be in the following format:
-#     => postgres://{user}:{password}@{host}:{port}/path
-# This is automatically configured on Heroku, you only need to worry if you also
-# want to run your app locally
-
 configure :production do
+  require 'newrelic_rpm'
   puts "[production environment]"
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
-  )
 end
 
-configure :development do
-  puts "[develoment Environment]"
-end
-
-configure :test do
-  puts "[test Environment]"
+configure :development, :test do
+  puts "[develoment or test Environment]"
 end
