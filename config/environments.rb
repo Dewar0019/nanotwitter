26d6ed7e5dbde9f6916d2379e255ef39e7549f63
lttviet <lttviet@gmail.com>
2015-11-08 22:41:03 -0500

    bugfix

--- config/environments.rb
+++ config/environments.rb
@@ -1,6 +1,17 @@
 configure :production do
   require 'newrelic_rpm'
   puts "[production environment]"
+
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+  )
 end
 
 configure :development, :test do
